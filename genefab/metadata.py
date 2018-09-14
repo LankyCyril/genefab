85460f1939cc4e3f072a68b495d5ae5c93fd8459
LankyCyril <cyril@ro.ru>
2018-09-14 17:33:38 -0400

    get_data_urls() from data/study/filelistings

--- genefab/metadata.py
+++ genefab/metadata.py
@@ -1,11 +1,7 @@
 from urllib.request import urlopen
 from json import loads
-from copy import deepcopy
-from numpy import nan
-from re import sub, search
+from re import sub
 from pandas import DataFrame, concat
-from argparse import Namespace
-from warnings import warn
 
 URL_ROOT = "https://genelab-data.ndc.nasa.gov/genelab"
 
@@ -13,64 +9,6 @@ def get_json(url):
     """HTTP get, decode, parse"""
     with urlopen(url) as response:
         return loads(response.read().decode())
-
-def get_hits_json(max_hit_count=10000):
-    """Get all possible 'hits' to projects from the GLDS database"""
-    poller_url = "{}/data/search?term=GLDS&type=cgene&size={}"
-    url = poller_url.format(URL_ROOT, max_hit_count)
-    meta = get_json(url)
-    if meta["timed_out"]:
-        raise ConnectionError("Request timed out")
-    else:
-        try:
-            return meta["hits"]["hits"]
-        except KeyError:
-            raise ValueError("Unexpected JSON format")
-
-def humanize_hit_source(hit_source, na_values=(), sort_subfields=False):
-    """Flatten fields, sprinkle separators where necessary"""
-    hhs = deepcopy(hit_source)
-    hhs["Study Person"] = " ".join(hit_source["Study Person"].values())
-    hhs["Mission Start Date"] = hit_source["Mission"]["Start Date"]
-    hhs["Mission End Date"] = hit_source["Mission"]["End Date"]
-    hhs["Mission Name"] = hit_source["Mission"]["Name"]
-    del hhs["Mission"]
-    for key, value in hhs.items():
-        if not isinstance(value, str):
-            err = "{} field {} is not a string".format(hhs["Accession"], key)
-            raise ValueError(err)
-        else:
-            if value in na_values:
-                value = nan
-            elif "  " in value:
-                # NB! This conversion is crude and does not yet account for
-                # many edge cases, or 'parallel' subfields in different fields
-                value = sub(r'\s\s+', ";", value)
-                if sort_subfields:
-                    subfields = sorted(value.split(";"))
-                    value = ";".join(subfields)
-            hhs[key] = value
-    return hhs
-
-def hits_to_dataframe(hits, na_values=("NA", "NaN", ""), sort_subfields=True):
-    """Find all fields that exist in sources, combine all data into one dataframe"""
-    fields = set()
-    humanized_sources = []
-    for hit in hits:
-        hs = humanize_hit_source(hit["_source"], na_values, sort_subfields)
-        fields |= set(hs.keys())
-        humanized_sources.append(hs)
-    meta_table = DataFrame(
-        columns=sorted(fields), index=range(len(humanized_sources))
-    )
-    for i, hs in enumerate(humanized_sources):
-        for key, value in hs.items():
-            meta_table.loc[i, key] = value
-    if "Accession" in meta_table.columns:
-        meta_table.index = meta_table["Accession"]
-        return meta_table
-    else:
-        raise KeyError("Key 'Accession' missing from metadata")
 
 class GeneLabDataSet():
     accession = None
@@ -83,8 +21,10 @@ class GeneLabDataSet():
         data_json = get_json(getter_url.format(URL_ROOT, accession))
         if len(data_json) > 1:
             raise ValueError("Too many results returned, unexpected behavior")
+        self._json = data_json[0]
         try:
-            self._isa2json = data_json[0]["foreignFields"][0]["isa2json"]
+            self._internal_id = self._json["_id"]
+            self._isa2json = self._json["foreignFields"][0]["isa2json"]
             assay_data = self._isa2json["additionalInformation"]["assays"]
             assay_json = list(assay_data.values())[0]
             self._header = assay_json["header"]
@@ -148,27 +88,30 @@ class GeneLabDataSet():
             raise KeyError("No factor associated with dataset")
         return factors
     """"""
-    def get_datafiles(self, as_urls=True):
+    def get_file_table(self):
         """Return DataFrame subset to filenames and factor values"""
         factor_fields = self.get_factors(as_fields=True)
         factor_dataframe = self.frame()[list(factor_fields.values())]
         factor_dataframe.columns = list(factor_fields.keys())
         datafiles_field_id = self._field_name_to_id("Array Data File")
         datafiles_names = self.frame()[datafiles_field_id]
-        if as_urls:
-            datafiles_urls = datafiles_names.apply(
-                lambda fn: "{}/static/media/dataset/{}_microarray_{}.gz".format(
-                    URL_ROOT, self.accession, sub(r'^\*', "", fn)
-                )
+        datafiles_names = datafiles_names.apply(
+            lambda fn: "{}_microarray_{}.gz".format(
+                self.accession, sub(r'^\*', "", fn)
             )
-            datafiles_urls.name = "URL"
-            datafiles = concat([factor_dataframe, datafiles_urls], axis=1)
-        else:
-            datafiles_names = datafiles_names.apply(
-                lambda fn: "{}_microarray_{}.gz".format(
-                    self.accession, sub(r'^\*', "", fn)
-                )
-            )
-            datafiles_names.name = "filename"
-            datafiles = concat([factor_dataframe, datafiles_names], axis=1)
+        )
+        datafiles_names.name = "filename"
+        datafiles = concat([factor_dataframe, datafiles_names], axis=1)
         return datafiles
+    """"""
+    def get_data_urls(self):
+        """Return URLs of data files stored on GeneLab servers"""
+        listing_url = "{}/data/study/filelistings/{}".format(
+            URL_ROOT, self._internal_id
+        )
+        return {
+            record["file_name"]: "{}/static/media/dataset/{}".format(
+                URL_ROOT, record["file_name"]
+            )
+            for record in get_json(listing_url)
+        }
