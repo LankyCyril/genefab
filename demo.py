3131314fbb68aa857fec6712c9749b1bf4dfcc0b
LankyCyril <cyril@ro.ru>
2019-02-27 00:53:40 -0500

    Updated demo.py

--- demo.py
+++ demo.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 from genefab import get_datasets
 
-datasets = get_datasets(maxcount=1000, verbose=True)
-with_derived, successful = 0, 0
-failed_accessions = set()
+header = [
+    "#Accession", "AssayName", "Success", "MatrixRows", "MatrixCols", "Error"
+]
+print(*header, sep="\t")
+
+datasets = get_datasets(assay="transcription", maxcount=1000, verbose=True)
 
 for dataset in datasets:
     for assay in dataset.assays:
         if assay.available_derived_file_types:
-            with_derived += 1
             try:
                 matrix = assay.get_combined_matrix()
             except Exception as e:
-                print("FAIL:", dataset.accession, assay.name, e)
-                failed_accessions.add(dataset.accession)
+                success = False
+                error = repr(e)
+                shape = (-1, -1)
             else:
-                print("SUCCESS:", dataset.accession, assay.name, matrix.shape)
-                successful += 1
-
-print("# total datasets found:", len(datasets))
-print("# assays with derived data:", with_derived)
-print("# successful matrices: {}/{}".format(successful, with_derived))
-print("# failed matrices: {}/{}".format(with_derived-successful, with_derived))
-
-print("Problematic datasets:")
-print(*sorted(failed_accessions), sep=", ")
+                success = True
+                error = None
+                shape = matrix.shape
+            fields = [
+                dataset.accession, assay.name, success, *shape, error
+            ]
+            print(*fields, sep="\t", flush=True)
