d81e5a6aee5478b81fbbb9a943edb80bf792f942
LankyCyril <cyril@ro.ru>
2019-04-02 10:39:41 -0400

    Removed LegacyAssay (for now)

--- genefab/__init__.py
+++ genefab/__init__.py
@@ -1,5 +1,4 @@
 from ._dataset import get_datasets, GeneLabDataSet
 from ._assay import Assay
-from ._legacy_assay import PerSampleMatrix, CompoundMatrix, Assay as LegacyAssay
 from ._exceptions import GeneLabJSONException
 GLDS = GeneLabDataSet
