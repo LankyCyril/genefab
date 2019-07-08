181f67ee453378fc683b1805a7f51dd3fbb18735
LankyCyril <cyril@ro.ru>
2019-07-08 15:25:00 -0400

    Set proper content type

--- gf.py
+++ gf.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, Response
+from genefab import GLDS
+from json import dumps
 
+app = Flask("genefab")
+
+@app.route("/")
 def hello_space():
     return "Hello, space!"
 
-app = Flask("genefab")
-app.route("/")(hello_space)
+@app.route("/<accession>.json")
+def glds_json(accession):
+    glds = GLDS(accession)
+    return Response(dumps([glds._json]), mimetype="text/json")
+
+@app.route("/<accession>.tsv")
+def glds_tsv(accession):
+    glds = GLDS(accession)
+    return Response(
+        glds.assays._as_dataframe.to_csv(sep="\t"),
+        mimetype="text/plain"
+    )
