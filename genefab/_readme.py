860b996d943eb459fdd1a0e553bec8a42f88a1df
LankyCyril <cyril@ro.ru>
2019-11-06 16:40:50 -0500

    Reference: some formatting

--- genefab/_readme.py
+++ genefab/_readme.py
@@ -41,13 +41,7 @@ html = """
         <h1>NASA GeneLab Data API reference</h1>
             <h2><a name='request_anatomy'>The anatomy of a request</a></h2>
                 <span class='code'>
-                    <tt>{url_root}</tt> <tt>/</tt>
-                    <a style='background:#eedd55;' href='#dataset_accession'>dataset_accession</a> /
-                    <a style='background:#55dd55;' href='#assay_name'>assay_name</a> /
-                    <a style='background:#dd5555;' href='#data_category'>data_category</a> /
-                    <a style='background:#aaaaff;' href='#data_type'>data_type</a> /
-                    <a style='background:#ff88ff;' href='#transform'>transform</a> /?
-                    <a style='background:#cccccc;' href='#get_args'>[get_args]</a>
+                    <tt>{url_root}</tt><tt>/</tt><a style='background:#eedd55;' href='#dataset_accession'>dataset_accession</a>/<a style='background:#55dd55;' href='#assay_name'>assay_name</a>/<a style='background:#dd5555;' href='#data_category'>data_category</a>/<a style='background:#aaaaff;' href='#data_type'>data_type</a>/<a style='background:#ff88ff;' href='#transform'>transform</a>/?<a style='background:#cccccc;' href='#get_args'>[get_args]</a>
                 </span>
                 <p>
                     The request can be routed to any depth. All following URLs
@@ -58,7 +52,7 @@ html = """
                     <a href='{url_root}/GLDS-4/assay/?fmt=html'>{url_root}/<span style='background:#eedd55'>GLDS-4</span>/<span style='background:#55dd55'>assay</span>/?<span style='background:#cccccc'>fmt=html</span></a><br>
                     <a href='{url_root}/GLDS-42/assay/annotation/?fmt=json'>{url_root}/<span style='background:#eedd55'>GLDS-42</span>/<span style='background:#55dd55'>assay</span>/<span style='background:#dd5555'>annotation</span>/?<span style='background:#cccccc'>fmt=json</span></a><br>
                     <a href='{url_root}/GLDS-30/a_GLDS-30_microarray_metadata-txt/data/pca/'>{url_root}/<span style='background:#eedd55'>GLDS-30</span>/<span style='background:#55dd55'>a_GLDS-30_microarray_metadata-txt</span>/<span style='background:#dd5555'>data</span>/<span style='background:#aaaaff'>pca</span>/</a><br>
-                    <a href='{url_root}/GLDS-63/assay/data/processed/descriptive/?sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose&hidecol=Protocol%20REF&top=20'>{url_root}/<span style='background:#eedd55'>GLDS-63</span>/<span style='background:#55dd55'>assay</span>/<span style='background:#dd5555'>data</span>/<span style='background:#aaaaff'>processed</span>/<span style='background:#ff88ff'>descriptive</span>/?<span style='background:#cccccc'>sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose&hidecol=Protocol%20REF&top=20</span></a>
+                    <a href='{url_root}/GLDS-63/assay/data/processed/descriptive/?sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose&hidecol=Protocol%20REF&top=20'>{url_root}/<span style='background:#eedd55'>GLDS-63</span>/<span style='background:#55dd55'>assay</span>/<span style='background:#dd5555'>data</span>/<span style='background:#aaaaff'>processed</span>/<span style='background:#ff88ff'>descriptive</span>/?<span style='background:#cccccc'>sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose</span>&<span style='background:#cccccc'>hidecol=Protocol%20REF</span>&<span style='background:#cccccc'>top=20</span></a>
                 </code>
             <br><br><hr>
             <h3>{url_root}/<a style='background:#eedd55' name='dataset_accession'>dataset_accession</a></h3>
