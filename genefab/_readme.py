42e7b3bed8467b09081dfd8bfdd7b7101f4a1f05
LankyCyril <cyril@ro.ru>
2019-11-06 14:20:25 -0500

    Reference: description of dataset_accession

--- genefab/_readme.py
+++ genefab/_readme.py
@@ -12,6 +12,14 @@ html = """
             }}
             span.code a, span.code tt {{
                 font-family: monospace, mono;
+                color: #000;
+                text-decoration: none;
+            }}
+            a {{
+                font-family: monospace, mono;
+                font-size: .85em;
+                color: #000;
+                text-decoration: none;
             }}
             code {{
                 font-family: monospace, mono;
@@ -20,6 +28,11 @@ html = """
             code a {{
                 color: #000;
                 text-decoration: none;
+                font-size: 1em;
+            }}
+            h3 {{
+                font-family: monospace, mono;
+                font-size: 1.3em;
             }}
         </style>
     </head>
@@ -29,7 +42,7 @@ html = """
             <h2><a name='request_anatomy'>The anatomy of a request</a></h2>
                 <span class='code'>
                     <tt>{url_root}</tt> <tt>/</tt>
-                    <a style='background:#eedd55;'>dataset_accession</a> /
+                    <a style='background:#eedd55;' href='#dataset_accession'>dataset_accession</a> /
                     <a style='background:#55dd55;'>assay_name</a> /
                     <a style='background:#dd5555;'>data_category</a> /
                     <a style='background:#aaaaff;'>data_type</a> /
@@ -47,6 +60,16 @@ html = """
                     <a href='{url_root}/GLDS-30/a_GLDS-30_microarray_metadata-txt/data/pca/'>{url_root}/<span style='background:#eedd55'>GLDS-30</span>/<span style='background:#55dd55'>a_GLDS-30_microarray_metadata-txt</span>/<span style='background:#dd5555'>data</span>/<span style='background:#aaaaff'>pca</span>/</a><br>
                     <a href='{url_root}/GLDS-63/assay/data/processed/descriptive/?sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose&hidecol=Protocol%20REF&top=20'>{url_root}/<span style='background:#eedd55'>GLDS-63</span>/<span style='background:#55dd55'>assay</span>/<span style='background:#dd5555'>data</span>/<span style='background:#aaaaff'>processed</span>/<span style='background:#ff88ff'>descriptive</span>/?<span style='background:#cccccc'>sort_by=Parameter%20Value:%20Absorbed%20Radiation%20Dose&hidecol=Protocol%20REF&top=20</span></a>
                 </code>
+            <br><br><hr>
+            <h3>{url_root}/<a style='background:#eedd55' name='dataset_accession'>dataset_accession</a></h3>
+                <b>Field</b>: <tt>dataset_accession</tt><br>
+                <b>Type</b>: string<br>
+                <b>Regex</b>: <code>GLDS-[0-9]+</code><br>
+                <b>Description</b>: Returns information about the dataset and
+                the assays it contains. Columns: resource type, name, factors
+                associated with it, file types available, protocols used.<br>
+                <b>Example</b>:
+                <a href='{url_root}/GLDS-111/?fmt=html'>{url_root}/<span style='background:#eedd55'>GLDS-111</span>/?<span style='background:#cccccc'>fmt=html</span></a><br>
     </body>
 </html>
 """
